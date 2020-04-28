98f13df4203e5574066b76744e94a57749dabaf1
ferdyanggara <moeharyoso89@gmail.com>
2020-04-28 19:03:59 +0700

    fourth commit news

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-new Chart(document.getElementById('line-chart'), {
-  type: 'line',
-  data: {
-    labels: stockChartXValuesFunction,
-    datasets: [
-      {
-        data: stockChartYValuesFunction,
-        label: `${stockName.value} stock`,
-        borderColor: '#3e95cd',
-        fill: false,
-      },
-    ],
-  },
-  options: {
-    title: {
-      display: true,
-      text: 'Your Stock',
-    },
-  },
-});
+class App {
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+}
+
+// async function fetchStock() {
+// 	const API_KEY = 'LCBLU10TT28NF5NQ';
+// 	let API_CALL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockName.value}&outputsize=compact&apikey=${API_KEY}`;
+// 	console.log(API_CALL);
+// 	stockChartXValuesFunction = [];
+// 	stockChartYValuesFunction = [];
+// 	await fetch(API_CALL)
+// 		.then((response) => response.json())
+// 		.then(function (data) {
+// 			console.log(data);
+// 			for (var key in data['Time Series (Daily)']) {
+// 				stockChartXValuesFunction.push(key);
+// 				stockChartYValuesFunction.push(
+// 					data['Time Series (Daily)'][key]['1. open']
+// 				);
+// 			}
+// 		});
+
+// 	console.log(stockChartXValuesFunction);
+// 	console.log(stockChartYValuesFunction);
+// }
--- script.js
+++ script.js
@@ -1,116 +1,153 @@
+const http = new App();
+
 // Define UI Vars
 const form = document.getElementById('form');
 const stockName = document.getElementById('stockname');
 const duration = document.getElementById('duration');
 const amount = document.getElementById('amount');
+const newsList = document.getElementById('news-list');
+const stocksMovement = document.getElementById('stocksmovement');
 
 form.addEventListener('submit', addStock);
 
 let stockLabelArray = [];
 let investmentAmount = [];
 let investmentDuration = [];
+var stockChartXValuesFunction = [];
+var stockChartYValuesFunction = [];
+var test = [];
+
+var color = [
+  'rgba(255, 99, 132, 1)',
+  'rgba(54, 162, 235, 1)',
+  'rgba(255, 206, 86, 1)',
+  'rgba(75, 192, 192, 1)',
+  'rgba(153, 102, 255, 1)',
+  'rgba(153, 102, 255, 1)',
+];
+var index = 0;
+
+// fetch news
+function retrieve(e) {
+  const apiKey = '40fb5e0404e345999353d4db49879216';
+
+  let topic = stockName.value;
+
+  let apiCall = `http://newsapi.org/v2/everything?q=${topic}&from=2020-04-27&to=2020-04-27&sortBy=popularity&pageSize=5&apiKey=${apiKey}`;
+
+  // get news
+  https: http
+    .get(apiCall)
+    .then(function (data) {
+      data.articles.forEach((article) => {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        a.setAttribute('href', article.url);
+        a.setAttribute('target', '_blank');
+        a.textContent = article.title;
+        li.appendChild(a);
+        newsList.appendChild(li);
+      });
+    })
+    .catch((err) => console.log(err));
+}
+
+// fetch stock
+function fetchStock() {
+  const API_KEY = 'MWXNGW5ZXCM5N88P';
+  let API_CALL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockName.value}&outputsize=compact&apikey=${API_KEY}`;
+
+  // Get Users
+  http
+    .get(API_CALL)
+    .then(function (data) {
+      for (var key in data['Time Series (Daily)']) {
+        stockChartXValuesFunction.unshift(key);
+        stockChartYValuesFunction.push(
+          data['Time Series (Daily)'][key]['1. open']
+        );
+      }
+    })
+    .catch((err) => console.log(err));
+}
 
 function addStock(e) {
+  stockChartYValuesFunction = [];
+  stockChartXValuesFunction = [];
+  fetchStock();
+  retrieve();
   stockLabelArray.push(stockName.value);
   investmentAmount.push(amount.value);
+  // Doughnut Chart
+  var ctx = document.getElementById('myChart').getContext('2d');
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: stockLabelArray,
 
-  // Fetch API
+      datasets: [
+        {
+          label: '# of Votes',
+          data: investmentAmount,
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 0,
+        },
+      ],
+    },
+  });
 
-  function fetchStock() {
-    const API_KEY = 'LCBLU10TT28NF5NQ';
-    let API_CALL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockName.value}&outputsize=compact&apikey=${API_KEY}`;
-    console.log(API_CALL);
-    var stockChartXValuesFunction = [];
-    var stockChartYValuesFunction = [];
-    fetch(API_CALL)
-      .then((response) => response.json())
-      .then(function (data) {
-        console.log(data);
-        for (var key in data['Time Series (Daily)']) {
-          stockChartXValuesFunction.push(key);
-          stockChartYValuesFunction.push(
-            data['Time Series (Daily)'][key]['1. open']
-          );
-        }
-      });
-
-    console.log(stockChartXValuesFunction);
-    console.log(stockChartYValuesFunction);
+  var data = {
+    data: stockChartYValuesFunction,
+    borderColor: color[index],
+    label: `${stockName.value} stock`,
+    fill: false,
+  };
+  function addData(chart, label, color, data) {
+    chart.data.datasets.forEach((dataset) => {
+      dataset.data.push(data);
+    });
+    chart.update();
   }
 
-  fetchStock();
-
-  // Doughnut Chart
-  // var ctx = document.getElementById('myChart').getContext('2d');
-  // new Chart(ctx, {
-  //   type: 'doughnut',
-  //   data: {
-  //     labels: stockLabelArray,
-
-  //     datasets: [
-  //       {
-  //         label: '# of Votes',
-  //         data: investmentAmount,
-  //         backgroundColor: [
-  //           'rgba(255, 99, 132, 1)',
-  //           'rgba(54, 162, 235, 1)',
-  //           'rgba(255, 206, 86, 1)',
-  //           'rgba(75, 192, 192, 1)',
-  //           'rgba(153, 102, 255, 1)',
-  //           'rgba(255, 159, 64, 1)',
-  //         ],
-  //         borderColor: [
-  //           'rgba(255, 99, 132, 1)',
-  //           'rgba(54, 162, 235, 1)',
-  //           'rgba(255, 206, 86, 1)',
-  //           'rgba(75, 192, 192, 1)',
-  //           'rgba(153, 102, 255, 1)',
-  //           'rgba(255, 159, 64, 1)',
-  //         ],
-  //         borderWidth: 0,
-  //       },
-  //     ],
-  //   },
-  // });
-
-  // let data = [
-  //   {
-  //     data: stockChartYValuesFunction,
-  //     label: `${stockName.value} stock`,
-  //     borderColor: '#3e95cd',
-  //     fill: false,
-  //   },
-  // ];
-
-  // data.push(obj);
-
-  // console.log(data);
+  test.push(data);
 
   // Line Chart
-  // new Chart(document.getElementById(`line-chart`), {
-  //   type: 'line',
-  //   data: {
-  //     labels: stockChartXValuesFunction,
-  //     datasets: [
-  //       {
-  //         data: stockChartYValuesFunction,
-  //         label: `${stockName.value} stock`,
-  //         borderColor: '#3e95cd',
-  //         fill: false,
-  //       },
-  //     ],
-  //   },
-  //   options: {
-  //     title: {
-  //       display: true,
-  //       text: 'Your Stock',
-  //     },
-  //   },
-  // });
-
+  var lineChart = new Chart(document.getElementById(`line-chart`), {
+    type: 'line',
+    data: {
+      labels: stockChartXValuesFunction,
+      datasets: test,
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Your Stock',
+      },
+    },
+  });
+  addData(lineChart, stockName.value, stockChartYValuesFunction);
   // lanjutan atas
-  stockName.remove(stockName.selectedIndex);
 
+  index += 1;
+  // if(index => 5){
+  //   index = 0;
+  // }
+  stockName.remove(stockName.selectedIndex);
   stockName.value = '';
   duration.value = '';
   amount.value = '';
@@ -118,56 +155,10 @@ function addStock(e) {
   e.preventDefault();
 }
 
-// function createJson() {
-//   let obj = {
-//     data: stockChartYValuesFunction,
-//     label: `AMZN stock2`,
-//     borderColor: '#3e95cd',
-//     fill: false,
-//   };
-// }
+// function retrieve(e) {
+//   const apiKey = '40fb5e0404e345999353d4db49879216';
 
-// function storeTaskInLocalStorage(task) {
-//   let tasks;
-//   if (localStorage.getItem('tasks') === null) {
-//     tasks = [];
-//   } else {
-//     tasks = JSON.parse(localStorage.getItem('tasks')); //the "|| []" replaces possible null from localStorage with empty array
-//     if (tasks.indexOf(task) == -1) {
-//       tasks.push(task);
-//       localStorage.setItem('tasks', JSON.stringify(tasks));
-//     }
-//   }
-//   tasks.push(task);
-//   localStorage.setItem('tasks', JSON.stringify(tasks));
-// }
+//   let topic = stockName.value;
 
-// function getTasks() {
-//   let tasks;
-//   if (localStorage.getItem('tasks') === null) {
-//     tasks = [];
-//   } else {
-//     tasks = JSON.parse(localStorage.getItem('tasks'));
-//   }
-
-//   tasks.forEach(function (task) {
-//     // create li elemnt
-//     const p = document.createElement('p');
-//     console.log(!localStorage.getItem('tasks').includes(task));
-//     if (!localStorage.getItem('tasks').includes(task)) {
-//       p.appendChild(document.createTextNode(task));
-//       output.appendChild(p);
-//     }
-//   });
-// }
-
-// function removeTask(e) {
-//   if (e.target.parentElement.classList.contains('delete-item')) {
-//     if (confirm('are you sure')) {
-//       e.target.parentElement.parentElement.remove();
-
-//       //   remove from lS
-//       removeTaskFromLocalStorage(e.target.parentElement.parentElement);
-//     }
-//   }
+//   let url = `http://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`;
 // }
